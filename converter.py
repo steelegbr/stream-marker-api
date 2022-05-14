1af934ce1063e0f26471d51d5cf6b0443028c26a
steelegbr <steelegbr@gmail.com>
2022-05-14 20:24:02 +0100

    Added better descriptions of the fields into the API.

--- converter.py
+++ converter.py
@@ -12,21 +12,3 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-
-from fastapi import FastAPI, File, Form, UploadFile
-
-app = FastAPI()
-
-
-@app.post("/")
-async def convert_file(
-    stream: UploadFile = File(..., title="A captured stream to extract markers from"),
-    metadata_interval: int = Form(
-        ..., ge=1, title="The icy-metaint value from Shoutcast"
-    ),
-    bitrate: int = Form(..., ge=16, title="Stream bitrate in kbps"),
-    encoding: str = Form(
-        ..., regex="^audio\/\w+$", title="Content-Type headed for the stream"
-    ),
-):
-    return {"filename": stream.filename}
